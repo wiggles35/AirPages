5fed2ff422b6c8f03010b69228e7282dacacc621
Tyler Krasny <tekrasny@gmail.om>
2021-04-28 18:51:00 -0400

    Commiting before I bork my pretty css grid

--- frontend/src/Components/homepage/Homepage.js
+++ frontend/src/Components/homepage/Homepage.js
@@ -2,15 +2,17 @@ import React from "react";
 import "./homepage.css";
 import FileUpload from "../../Common/Services/fileUpload.js"
 import Facts from "../facts/Facts.js"
+import Post from "../posts/Post.js"
 
 export function Homepage() {
 
     return (
         <main id="homepage" className="homepageContainer">
             <div className="pageFiles">
-                <h1>Upload an image to AirPages</h1>
+                <h1>Create a post on AirPages</h1>
                 <br />
                 <FileUpload />
+                <Post />
             </div>
             <div className="pageData">
                 <h1>Enter Directory Information</h1>
